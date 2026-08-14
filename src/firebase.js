a2659bc2d3bf90bc3433f6699cbb73320273af07
kyawzinaung4105-collab <kyawzinaung4105@gmail.com>
2026-08-14 17:00:13 +0630

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,15 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCMuEbPcTT9j-WvNLAWcAX3nJr_-x1uFo",
-  authDomain: "fieldbooking-80ad6.firebaseapp.com",
-  projectId: "fieldbooking-80ad6",
-  storageBucket: "fieldbooking-80ad6.appspot.com",
-  messagingSenderId: "297623698493",
-  appId: "1:297623698493:web:49483a28305cba8abc7e54"
+  apiKey: 'AIzaSyCMuEbPcTT9j-WvNLAwCAX3nJr_-x1uFo',
+  authDomain: 'fieldbooking-80ad6.firebaseapp.com',
+  projectId: 'fieldbooking-80ad6',
+  storageBucket: 'fieldbooking-80ad6.firebasestorage.app',
+  messagingSenderId: '297623698493',
+  appId: '1:297623698493:web:49483a28305cba8abc7e54'
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const db = getFirestore(app);
 export const auth = getAuth(app);
